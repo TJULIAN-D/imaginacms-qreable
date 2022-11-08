e1c7f4bcf7a06266a1d4a2258a453998ec203eec
Juan Escalante <jcec007@gmail.com>
2022-11-08 17:42:51 -0500

    Update: new features multi tenant mode +Add: Ibuilder module

--- Services/QrService.php
+++ Services/QrService.php
@@ -15,20 +15,22 @@ class QrService
 
     }
 
-    public function addQr($model, $redirect, $zone = null){
-        $entityClass = get_class($model);
-    $qredRepository = app("Modules\Qreable\Repositories\QredRepository");
-    $qReables = $qredRepository->getItemsBy(json_decode(json_encode(['filter' => ['qreableId' => $model->id,'qreableType' => $entityClass,'zone' => $zone]])));
-    if ($qReables->isEmpty()) {
+    public function addQr($model, $redirect, $zone = null)
+    {
+      $entityClass = get_class($model);
+      $qredRepository = app("Modules\Qreable\Repositories\QredRepository");
+      $qReables = $qredRepository->getItemsBy(json_decode(json_encode(['filter' => ['qreableId' => $model->id, 'qreableType' => $entityClass, 'zone' => $zone]])));
+      if ($qReables->isEmpty()) {
         $qr = new Qr(['code' => '']);
         $qr->save();
         if ($qr->qreables($entityClass)->get()->contains($model->id) === false) {
-            $qr->qreables($entityClass)->attach($model, ['zone' => $zone, 'redirect' => $redirect]);
-            $lastQreable = $qr->qreablesByZone($entityClass, $zone)->first();
-            $qrCode = $this->generateQrCode(route('api.qreable.show',[$lastQreable->pivot->id]));
-            $qr->update(['code'=>$qrCode]);
+          $qr->qreables($entityClass)->attach($model, ['zone' => $zone, 'redirect' => $redirect]);
+          $lastQreable = $qr->qreablesByZone($entityClass, $zone)->first();
+          $qrCode = $this->generateQrCode(route('api.qreable.show', [$lastQreable->pivot->id]));
+          $qr->update(['code' => $qrCode]);
         }
-
+    
+      }
     }
   
     public function generateQrCode($code){
